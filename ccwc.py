c89f5057bebc86e391442a6afbccd6d70d0b3de1
RiazK <RiazCKhan@gmail.com>
2024-06-12 15:07:23 -0400

    add action true to optional args

--- ccwc.py
+++ ccwc.py
@@ -3,32 +3,39 @@ from functions import byte_count, line_count, word_count, char_count
 
 parser = argparse.ArgumentParser()
 parser.add_argument(
+    "filename",
+    help="The number of bytes, lines, and words from input file.",
+)
+parser.add_argument(
     "-c",
-    "--byte",
     help="The number of bytes in each input file is written to the standard output.",
+    action="store_true"
 )
 parser.add_argument(
     "-l",
-    "--line",
     help="The number of lines in each input file is written to the standard output.",
+    action="store_true"
 )
 parser.add_argument(
     "-w",
-    "--word",
+    "-word",
     help="The number of words in each input file is written to the standard output.",
+    action="store_true"
 )
 parser.add_argument(
     "-m",
-    "--char",
+    "-char",
     help="The number of characters in each input file is written to the standard output.",
+    action="store_true"
 )
 args = parser.parse_args()
 
-if args.byte:
-    byte_count(args.byte)
-elif args.line:
-    line_count(args.line)
-elif args.word:
-    word_count(args.word)
-elif args.char:
-    char_count(args.char)
+
+if args.c:
+    byte_count(args.filename)
+# elif args.l:
+#     line_count(args.l)
+# elif args.w:
+#     word_count(args.w)
+# elif args.m:
+#     char_count(args.m)
